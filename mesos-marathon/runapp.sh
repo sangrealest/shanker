aff38e8520292fc71f26d7f3193405ecfe328910
Shanker <shanker@yeah.net>
2016-06-12 14:12:57 +0000

    update

--- mesos-marathon/runapp.sh
+++ mesos-marathon/runapp.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-curl -i -H 'Content-Type: application/json' -d@"$1" 192.168.0.33:8080/v2/apps
+if [ "$#" -ne 1 ]
+then
+    echo "You have to input one json file"
+    exit 1;
+fi
+curl -X POST -H 'Content-Type: application/json' -d@"$@" 192.168.0.33:8080/v2/apps
